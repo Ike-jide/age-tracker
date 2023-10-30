d99b98b62baa496a59ee41f90c3dbf18ca777406
ike-jide <ikejideofor@gmail.com>
2023-10-30 21:10:39 +0100

    added state management

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './componets/User/AddUser';
 import UserList from './componets/User/UserList';
 
 
 function App() {
+  const [userList, setUserList]= useState('')
   return (
     <div>
     <AddUser/>
